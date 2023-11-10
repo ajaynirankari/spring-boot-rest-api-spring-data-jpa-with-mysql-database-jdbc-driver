042b4d9236fb78fe275f231aaf26985c91e87887
Ajay.Nirankari <ajaykumarnirankarigl@gmail.com>
2023-11-10 10:38:19 +0530

    Remove unused imports

--- src/main/java/com/gl/UserManagementApp/UserManagementAppApplication.java
+++ src/main/java/com/gl/UserManagementApp/UserManagementAppApplication.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
